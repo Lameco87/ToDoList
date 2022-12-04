1645e2e92bbb203bfc71a0ac139652d459d3c91e
dRibeiro <diogoribeiro87@sapo.pt>
2022-12-04 00:27:54 +0000

    Verion 1 - Base Code only with HomePage witch is a Scaffold with yellow backgroundcolor

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './pages/homepage.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: HomePage(),
+    );
+  }
+}
