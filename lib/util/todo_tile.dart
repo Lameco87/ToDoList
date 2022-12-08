e868ec45650a89a70d3be3e4a0669edeb8ce409e
dRibeiro <diogoribeiro87@sapo.pt>
2022-12-08 15:49:53 +0000

    Version 2 - Add Slidable Widget to remove tiles

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -15,7 +15,7 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
       child: Container(
         decoration: BoxDecoration(
           color: Colors.yellow,
